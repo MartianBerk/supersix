be88fe0fb5827ca249ff021e27ea47a02ad4304d
martianberk <baker.bsc@gmail.com>
2019-09-15 14:37:45 +0100

    progress

--- web/supersix/app/__init__.py
+++ web/supersix/app/__init__.py
@@ -7,4 +7,4 @@ from mylib.webapi import app, route
 @route("/matches")
 def index():
     matches = MatchService().list(filters={"match_date": datetime.now().date()})
-    return {"matches": [m.to_dict() for m in matches]}
+    return {"matches": [m.to_dict(keys=["home_team", "away_team", "home_score", "away_score"]) for m in matches]}
