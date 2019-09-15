3f0e945a4d9a2bb94082f4a1a58c563a9fedff94
martianberk <baker.bsc@gmail.com>
2019-09-15 13:34:12 +0100

    add runtime seting:

--- python/mylib/supersix/process/scoreextractor.py
+++ python/mylib/supersix/process/scoreextractor.py
@@ -1,5 +1,9 @@
 import requests
 
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
+from time import sleep
+
 from mylib.supersix.service import MatchService
 
 
@@ -7,9 +11,10 @@ class ScoreExtractor:
     _URL = "https://api.football-data.org/v2/competitions"
     _KEY = "9c3a13b8586d4ba9af6723ffa1e15c67"  # TODO: secure credential
 
-    def __init__(self, league, matchday):
+    def __init__(self, league, matchday, max_run_seconds=0):
         self._league = league
         self._matchday = matchday
+        self._max_run_seconds = max_run_seconds
 
         self._match_service = MatchService()
 
@@ -26,15 +31,32 @@ class ScoreExtractor:
 
     def process(self):
         print(f"extracting {self._league} scores for matchday {self._matchday}")
+        start = datetime.now()
 
-        for match in self._collect_matches():
-            home = match["score"]["fullTime"]["homeTeam"]
-            away = match["score"]["fullTime"]["awayTeam"]
+        while True:
+            for match in self._collect_matches():
+                home = match["score"]["fullTime"]["homeTeam"]
+                away = match["score"]["fullTime"]["awayTeam"]
 
-            match = self._match_service.get(match["id"])
+                match = self._match_service.get(match["id"])
 
-            match.home_score = home
-            match.away_score = away
+                match.home_score = home
+                match.away_score = away
 
-            self._match_service.update(match)
-            print(f"updated {match.home_team} ({match.home_score}) vs {match.away_team} ({match.away_score})")
+                self._match_service.update(match)
+                print(f"updated {match.home_team} ({match.home_score}) vs {match.away_team} ({match.away_score})")
+
+            if datetime.now() > start + timedelta(seconds=self._max_run_seconds):
+                break
+
+            sleep(3)  # throttle
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Score extractor")
+    parser.add_argument("-l", "--league", type=str, help="League code to extract")
+    parser.add_argument("-m", "--matchday", type=int, help="Matchday")
+    parser.add_argument("-s", "--max_run_seconds", type=int, default=0, help="Run for seconds.")
+    args = vars(parser.parse_args())
+
+    ScoreExtractor(**args).process()
