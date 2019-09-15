ea22ce481fb77e77171bc3a019a94ddd7c08ee1e
martianberk <baker.bsc@gmail.com>
2019-09-15 13:52:02 +0100

    matches api

--- web/supersix/app/__init__.py
+++ web/supersix/app/__init__.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
+from mylib.supersix.service import MatchService
 from mylib.webapi import app, route
 
 
-@route("/")
+@route("/matches")
 def index():
-    return "Hello World"
+    now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    matches = MatchService().list(columns=["home_team", "away_team", "home_score", "away_score"],
+                                  filters={"match_date": now})
+    return {"matches": [m.to_dict() for m in matches]}
