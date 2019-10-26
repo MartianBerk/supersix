079a166cd3154f5ff0d8b51d3bd55b434324d107
martianberk <baker.bsc@gmail.com>
2019-10-26 14:32:46 +0100

    arg

--- web/supersix/supersix.py
+++ web/supersix/supersix.py
@@ -5,7 +5,7 @@ from mylib.webapi import main
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Start Supersix Web API")
-    parser.add_argument("-h", "--host", type=str, help="Host IP")
+    parser.add_argument("--host", type=str, help="Host IP")
     args = vars(parser.parse_args())
 
     main(**args)
