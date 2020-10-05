d67fde6f2e5a0c3e514e6f6f3531f08949b98bb3
martianberk <baker.bsc@gmail.com>
2020-10-05 18:43:49 +0100

    fixes

--- python/mb/supersix/process/extractors/scoreextractor.py
+++ python/mb/supersix/process/extractors/scoreextractor.py
@@ -23,20 +23,20 @@ class ScoreExtractor:
         self._matchday = matchday
         self._max_run_seconds = max_run_seconds
 
-        self._connector = self._CONNECTORS[league.code]
+        self._connector = self._CONNECTORS[self._league.code]
         self._match_service = MatchService()
 
-    def _update_match(self, match):
-        match = self._match_service.get_from_external_id(match["id"])
+    def _update_match(self, match_data):
+        match = self._match_service.get_from_external_id(match_data["id"])
         if not match:
             return None
 
-        match.status = match["status"]
-        match.match_minute = match.get("minute") or 90
-        match.home_score = match["score"]["fullTime"]["homeTeam"]
-        match.away_score = match["score"]["fullTime"]["awayTeam"]
+        match.status = match_data["status"]
+        match.match_minute = match_data.get("minute") or 90
+        match.home_score = match_data["score"]["fullTime"]["homeTeam"]
+        match.away_score = match_data["score"]["fullTime"]["awayTeam"]
 
-        self._match_service.update(match)
+        return self._match_service.update(match)
 
     def process(self):
         print(f"extracting {self._league} scores for matchday {self._matchday or self._league.current_matchday}")
@@ -44,14 +44,14 @@ class ScoreExtractor:
 
         if self._matchday:
             for match in self._connector.collect_historical_scores(self._league, self._matchday):
-                self._update_match(match)
+                match = self._update_match(match)
                 print(f"updated {match.home_team} ({match.home_score}) vs {match.away_team} ({match.away_score})")
 
             return None
 
         while True:
             for match in self._connector.collect_scores(self._league):
-                self._update_match(match)
+                match = self._update_match(match)
                 print(f"updated {match.home_team} ({match.home_score}) vs {match.away_team} ({match.away_score})")
 
             if datetime.now() > start + timedelta(seconds=self._max_run_seconds):
