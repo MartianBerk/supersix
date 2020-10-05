f19d2e09c55262100a62cc4e3f1277446e250062
martianberk <baker.bsc@gmail.com>
2020-10-05 18:01:36 +0100

    agents

---
+++ agents/supersix/match-extractor.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+
+from mb.supersix.process.extractors.matchextractor import MatchExtractor
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Extract matches")
+    parser.add_argument("-m", "--matchdays_ahead", type=int, help="Matchdays ahead")
+    args = vars(parser.parse_args())
+
+    MatchExtractor(**args).process()
