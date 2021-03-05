70cc5352e15eab201cce0ea3baaf6f9e3b3890af
MartianBerk <baker.bsc@gmail.com>
2021-03-05 19:49:29 +0000

    Update routes.py

--- python/baked/lib/supersix/webapi/app/supersix/game/routes.py
+++ python/baked/lib/supersix/webapi/app/supersix/game/routes.py
@@ -75,7 +75,7 @@ def game_live_scores():
     prediction_service = PredictionService()
 
     for m in matches:
-        prediction_filters = [("match_id", "equalto", m.id), ("drop", "null")]
+        prediction_filters = [("match_id", "equalto", m.id), ("drop", "equalto", False)]
         predictions = prediction_service.list(prediction_filters)
 
         for p in predictions:
