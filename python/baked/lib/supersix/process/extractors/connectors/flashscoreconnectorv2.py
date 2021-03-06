3a4afdd96bba8ad1380ae2df339f482e2ec412e0
Martin Baker <martin.baker@rimes.com>
2021-03-06 21:27:06 +0000

    final tweaks

--- python/baked/lib/supersix/process/extractors/connectors/flashscoreconnectorv2.py
+++ python/baked/lib/supersix/process/extractors/connectors/flashscoreconnectorv2.py
@@ -88,7 +88,7 @@ class FlashScoreConnectorV2(AbstractConnector):
 
             if collect:
                 match_date_div = div.find("div", attrs={"class": ["event__time"]})
-                if match_date_div and match_date_div.starts_with("Post"):
+                if match_date_div and match_date_div.startswith("Post"):
                     # postponed match
                     continue
 
