b02437539849091ec7f08686c87b5de46205049b
martianberk <baker.bsc@gmail.com>
2021-04-05 08:24:27 +0100

    fixes and cleanup

--- python/baked/lib/supersix/process/extractors/matchextractor.py
+++ python/baked/lib/supersix/process/extractors/matchextractor.py
@@ -31,7 +31,7 @@ class MatchExtractor:
             print(f"extracting matches for {league.name}...")
 
             for match in self._connector.collect_matches(league, self._matchday, look_ahead=self._matchdays_ahead):
-                start_time = datetime.strptime(match["utcDate"], "%Y-%m-%dT%H:%M:%SZ")
+                start_time = datetime.strptime(match["utcDate"], "%Y-%m-%d %H:%M:%S")
 
                 match = Match(external_id=str(match["id"]),
                               league_id=league.id,
