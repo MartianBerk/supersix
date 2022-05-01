36d42609e69eba55653a0f2941a267a7db52f9b6
martianberk <baker.bsc@gmail.com>
2022-05-01 13:28:03 +0100

    permissions public

--- python/baked/lib/supersix/model/admin/user.py
+++ python/baked/lib/supersix/model/admin/user.py
@@ -41,7 +41,7 @@ class User(IUser):
 
     @classmethod
     def public_attributes(cls):
-        return ["email", "user_id", "player_id", "account", "firstname", "lastname", "id"]
+        return ["email", "user_id", "player_id", "account", "firstname", "lastname", "id", "permissions"]
 
     @classmethod
     def get_sql_datatype(cls, item):
