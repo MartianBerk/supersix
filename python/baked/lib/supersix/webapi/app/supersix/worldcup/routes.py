4697c98dd57c61f86fefbb3c4925345598e6f376
Martin <baker.bsc@gmail.com>
2022-11-15 08:57:49 +0000

    list predictions

--- python/baked/lib/supersix/webapi/app/supersix/worldcup/routes.py
+++ python/baked/lib/supersix/webapi/app/supersix/worldcup/routes.py
@@ -33,6 +33,23 @@ def worldcup_scores():
     return response({"scores": [s.to_dict() for s in scores]})
 
 
+@supersix.route("/getpredictions", permissions=["QATARHERO"], subdomains=["worldcup"], methods=["GET"])
+def worldcup_get_predictions():
+    # Can this even happen?
+    uid = request.cookies.get("bkuid")
+    if not uid:
+        return response({"error": True, "message": "Not logged in."})
+
+    user = UserService(APPLICATION).get_from_uid(int(uid))
+    filters = [
+        ("player_id", "equalto", user.data.qatar_hero_player_id),
+        ("drop", "equalto", False)
+    ]
+    predictions = WorldCupService().list_predictions(filters=filters)
+
+    return response({"predictions": [p.to_dict() for p in predictions]})
+
+
 @supersix.route("/getprediction", permissions=["QATARHERO"], subdomains=["worldcup"], methods=["GET"])
 def worldcup_get_prediction():
     game = request.args.get("gameId")
@@ -115,58 +132,3 @@ def worldcup_add_prediction():
         prediction = service.create_prediction(prediction)
 
     return response(prediction.to_dict())
-
-
-# @supersix.route("/scores", open_url=True, subdomains=["worldcup"], methods=["GET"])
-# def game_live_scores():
-#     match_date = request.args.get("matchDate")
-#     if not match_date:
-#         return response({"error": True, "message": "missing matchDate"})
-
-#     try:
-#         start_date = datetime.strptime(match_date, "%d-%m-%Y")
-#         end_date = start_date + timedelta(days=1)
-#     except ValueError:
-#         return {"error": True, "message": "invalid date format, expected dd-mm-yyyy"}
-
-#     filters = [("match_date", "greaterthanequalto", start_date),
-#                ("match_date", "lessthanequalto", end_date),
-#                ("use_match", "equalto", True)]
-
-#     matches = MatchService().list(filters=filters)
-#     matches.sort(key=lambda m: m.game_number)
-
-#     players = {p.id: {"id": p.id,
-#                       "name": f"{p.first_name} {p.last_name}",
-#                       "matches": [],
-#                       "score": 0} for p in PlayerService().list()}
-
-#     # get predictions for each player/match combination
-#     prediction_service = PredictionService()
-
-#     for m in matches:
-#         prediction_filters = [("match_id", "equalto", m.id), ("drop", "equalto", False)]
-#         predictions = prediction_service.list(prediction_filters)
-
-#         for p in predictions:
-#             player = players.get(p.player_id)
-#             if not player:
-#                 continue
-
-#             correct = True if any([
-#                 m.home_score is not None and m.home_score > m.away_score and p.prediction == "home",
-#                 m.away_score is not None and m.away_score > m.home_score and p.prediction == "away",
-#                 m.home_score is not None and m.home_score == m.away_score and p.prediction == "draw"
-#             ]) else False
-
-#             match = m.to_dict()
-#             (match.pop(k) for k in match.keys() if k not in ["home_team", "away_team"])
-#             match.update({"prediction": p.prediction, "correct": correct, "status": m.status})
-
-#             players[p.player_id]["score"] += 1 if correct else 0
-#             players[p.player_id]["matches"].append(match)
-
-#     players = [p for p in players.values()]
-#     players.sort(key=lambda x: x["score"], reverse=True)
-
-#     return response({"scores": players})
