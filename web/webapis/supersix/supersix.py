6de3bcbb9612aec008517c5f5fefe3236923caca
Martin <baker.bsc@gmail.com>
2025-09-22 21:09:50 +0100

    Support port in web api

--- web/webapis/supersix/supersix.py
+++ web/webapis/supersix/supersix.py
@@ -6,6 +6,7 @@ from baked.lib.supersix.webapi import SupersixApi
 if __name__ == "__main__":
     parser = ArgumentParser(description="Supersix Web API.")
     parser.add_argument("--host", type=str, required=False, help="Host")
+    parser.add_argument("--port", type=str, required=False, help="Port")
     args = parser.parse_args()
 
-    SupersixApi.run(standalone=True, host=args.host)
+    SupersixApi.run(standalone=True, host=args.host, port=args.port)
